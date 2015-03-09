7c631c685b130f68d4fdd572a038a3a41ce428f2
drew <drew@drewolson.org>
2015-03-09 18:32:50 -0500

    Add use brunch for asset compilation.

---
+++ brunch-config.js
@@ -0,0 +1,24 @@
+exports.config = {
+  files: {
+    javascripts: {
+      joinTo: 'js/app.js'
+    },
+    stylesheets: {
+      joinTo: 'css/app.css'
+    },
+    templates: {
+      joinTo: 'js/app.js'
+    }
+  },
+
+  paths: {
+    watched: ["web/static", "test/static"],
+    public: "priv/static"
+  },
+
+  plugins: {
+    ES6to5: {
+      ignore: [/^(vendor)/]
+    }
+  }
+};
--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,15 @@ use Mix.Config
 config :blox, Blox.Endpoint,
   http: [port: 4000],
   debug_errors: true,
-  cache_static_lookup: false
+  cache_static_lookup: false,
+  watchers: [
+    {Path.expand("node_modules/brunch/bin/brunch"), ["watch"]}
+  ],
+  live_reload: [
+    Path.expand("priv/static/js/app.js"),
+    Path.expand("priv/static/css/app.css"),
+    Path.expand("web/templates/**/*.eex")
+  ]
 
 config :phoenix, :code_reloader, true
 
