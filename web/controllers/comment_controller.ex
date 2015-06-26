e627c48af652046537740085648d93ac98503a49
drew <drew@drewolson.org>
2015-06-26 12:57:56 -0500

    Small refactoring.

--- web/controllers/comment_controller.ex
+++ web/controllers/comment_controller.ex
@@ -5,7 +5,7 @@ defmodule Blox.CommentController do
   alias Blox.Post
 
   plug :scrub_params, "comment" when action in [:create]
-  plug :find_post!
+  plug :find_post
   plug :action
 
   def create(conn, %{"comment" => params}) do
@@ -30,18 +30,16 @@ defmodule Blox.CommentController do
     redirect conn, to: post_path(conn, :show, post)
   end
 
-  def find_post!(conn, _key_values) do
-    post = Blox.Repo.get_by(Post, %{"id": Map.get(conn.params, "post_id")})
-    case post do
-      nil  -> find_post_error!(conn)
-      post -> assign(conn, :post, post)
-    end
-  end
+  def find_post(conn, _key_values) do
+    post = Blox.Repo.get(Post, Map.get(conn.params, "post_id"))
 
-  defp find_post_error!(conn) do
-    conn
-    |> put_flash(:error, "Post not found")
-    |> redirect(to: post_path(conn, :index))
-    |> halt
+    if post do
+      conn |> assign(:post, post)
+    else
+      conn
+      |> put_flash(:error, "Post not found")
+      |> redirect(to: post_path(conn, :index))
+      |> halt
+    end
   end
 end
